b792300ec9028df72221bca247e3e7199bc69e5d
DanielleSLRecordings <164554798+DanielleSLRecordings@users.noreply.github.com>
2024-06-06 14:43:03 +0100

    Update main.tf

--- Stack-Dependencies/main.tf
+++ Stack-Dependencies/main.tf
@@ -5,6 +5,7 @@ data "spacelift_space_by_path" "intermediate-repo" {
 output "space_id" {
   value = data.spacelift_space_by_path.intermediate-repo.id
 }
+
 resource "spacelift_stack" "infra" {
   branch       = "main"
   name         = "Infrastructure stack"
